f08793a31b52421271bbc7e1bb05f79bd3eec03b
Uzhastin-Nikita <fosfor20@mail.ru>
2021-02-16 00:09:34 +0300

    add new program

---
+++ programs/public/rent.py
@@ -0,0 +1,7 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+mc= minecraft.Minecraft.create()
+pos = mc.player.getTilePos()
+for a in range(1):
+    mc.setBlocks(pos.x,pos.y,pos.z,pos.x+50,pos.y+50,pos.z+50,block.AIR.id)
